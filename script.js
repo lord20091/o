37417730252485ac4e3eaa0fec804f1c5b1b5227
lord20091 <abdulazizbozorov009@gmail.com>
2024-04-05 15:27:09 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+let ism = prompt(`String typedagi malumot kiriting `);
+
+let num = prompt(`Number typedagi malumot kiriting `);
+
+let boolean = prompt(`Boolean typedagi malumot kiriting `);
+
+boolean = confirm(`Hamma malumotlarni togri kiritganingizga ishonchiz komilmi?` );
+
+alert(`Siz kiritgan ${ism} sozi string typega tegishli`);
+
+alert(`Siz kiritgan ${num} sozi number typega tegishli`);
+
+alert(`Siz kiritgan ${boolean} sozi boolean typega tegishli`);
+
